7828bbc1687f294787c02bc417ce6743e33acf1b
kromate <akpananthony33@gmail.com>
2021-11-13 16:28:17 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+  extend: {
+     spacing: {
+      128: '32rem',
+     },
+    colors:{
+     primary:'#4A545D'
+     }
+   },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
