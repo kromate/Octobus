9be78e7305e1ac726eafc259b8a17f3623232502
kromate <akpananthony33@gmail.com>
2022-03-15 15:42:18 +0100

    update

--- src/components/BottomModal.vue
+++ src/components/BottomModal.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="[ 'px-6 z-50 fixed bottom-0 pt-4 rounded-t-[40px] inset-x-0 bg-white justify-between w-full text-gray' ]">
-      <div class="w-20 h-1 rounded bg-gray mx-auto mb-6"/>
+  <div :class="[ open ?'h-screen rounded-none top-0':'rounded-t-[40px]', 'px-6 z-50 fixed  py-4 bottom-14  inset-x-0 bg-white justify-between w-full animated open' ]" @click="toggleModal">
+      <div :class="[ open?'hidden':'',  'w-20 h-1 rounded bg-gray mx-auto mb-6 open']"/>
       <p class="text-black text-xl font-semibold">Where do you want to go ?</p>
   <div class="inputContainer">
    <i class="fa-solid fa-location-crosshairs text-primary text-left text-2xl px-4 pt-6 w-20"></i>
@@ -10,9 +10,10 @@
     <i class="fa-solid fa-location-dot text-primary text-left text-2xl p-4 w-20"></i>
     <input v-model="userDestination" class="w-full rounded-xl p-3 bg-lightGray text-center text-gray mb-4 mt-2" placeholder="Where are you going?"  type="text" >
 </div>
-<div class="inputContainer">
-    <i class="fa-solid fa-location-dot text-primary text-left text-2xl p-4 w-20"></i>
-    <input v-model="userDestination" class="w-full rounded-xl p-3 bg-lightGray text-center text-gray mb-4 mt-2" placeholder="Where are you going?"  type="text" >
+<div class="inputContainer" :class="[ open?'':'hidden',]">
+   <button class=" bg-black btn w-full !text-white mt-10">
+       Search
+   </button>
 </div>
 </div>
 </template>
@@ -25,13 +26,21 @@ Name: "BottomModal",
 setup(){
     const open = ref(false)
 
+    const toggleModal = (el)=>{
+        if(el.target.className.includes('open')){
+            open.value = !open.value
+        }
+    }
+
     return {
-        open
+        open, toggleModal
     }
 }
 }
 </script>
 
-<style>
-
+<style scoped>
+.animated{
+transition: all 0.35s ease;
+}
 </style>
