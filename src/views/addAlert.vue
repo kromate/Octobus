35a5790f799988314eb4130b181eff13653a3337
kromate <akpananthony33@gmail.com>
2022-04-16 17:20:13 +0100

    --- Auto-Git Commit ---

--- src/views/addAlert.vue
+++ src/views/addAlert.vue
@@ -1,36 +1,36 @@
 <template>
-<div class="mt-16">
-<tab-group/>
+	<div class="mt-16">
+		<tab-group/>
 
-<select-menu placeholder='route'>
-  <template #icon>
-    <i class="fa-solid fa-location-dot text-primary text-base"></i>
-  </template>
-</select-menu>
-<select-menu>
-  <template #icon>
-    <img src="../assets/icons/route.svg"/>
-  </template>
-</select-menu>
-<select-menu>
-  <template #icon>
-    <img src="../assets/icons/date.svg"/>
-  </template>
-</select-menu>
-<select-menu>
-  <template #icon>
-    <img src="../assets/icons/clock.svg"/>
-  </template>
-</select-menu>
-<select-menu>
-  <template #icon>
-    <i class="fa-solid fa-bell text-primary text-base "></i>
-  </template>
-</select-menu>
+		<select-menu placeholder='route'>
+			<template #icon>
+				<i class="fa-solid fa-location-dot text-primary text-base"></i>
+			</template>
+		</select-menu>
+		<select-menu>
+			<template #icon>
+				<img src="../assets/icons/route.svg"/>
+			</template>
+		</select-menu>
+		<select-menu>
+			<template #icon>
+				<img src="../assets/icons/date.svg"/>
+			</template>
+		</select-menu>
+		<select-menu>
+			<template #icon>
+				<img src="../assets/icons/clock.svg"/>
+			</template>
+		</select-menu>
+		<select-menu>
+			<template #icon>
+				<i class="fa-solid fa-bell text-primary text-base "></i>
+			</template>
+		</select-menu>
 
 
 
-</div>
+	</div>
 
 </template>
 
@@ -38,11 +38,11 @@
 import SelectMenu from '../components/SelectMenu.vue'
 import TabGroup from '../components/TabGroup.vue'
 export default {
- name:'AddAlertPage',
-  components: { TabGroup, SelectMenu },
-  setup(){
+	name:'AddAlertPage',
+	components: { TabGroup, SelectMenu },
+	setup(){
 
-  }
+	}
 
 }
 </script>
