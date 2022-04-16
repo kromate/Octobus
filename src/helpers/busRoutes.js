f31ef54c058cfe68b14db623e90df60c8245f93a
kromate <akpananthony33@gmail.com>
2022-04-16 14:32:19 +0100

    --- Auto-Git Commit ---

--- src/helpers/busRoutes.js
+++ src/helpers/busRoutes.js
@@ -43,33 +43,159 @@ export const AllBusRoute = {
 		},
 		{cord: '35.2297737,33.3246781', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
 		},
+	],
+    
+	'Gönyeli 1': [
+		{cord: '35.2297781,33.3242986', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
+		},
+		{cord: '35.2260295,33.3256762', name: 'Near East Bank Durağı.'
+		},
+		{cord: '35.2095115,33.2983604', name: 'İR-MAR Market Karşı Durağı.'
+		},
+		{cord: '35.2087531,33.3074487', name: 'Talihsiz Mobilya Durağı.'
+		},
+		{cord: '35.2118788,33.3071764', name: 'Ersoy Hoca Apt. Durağı.'
+		},
+		{cord: '35.2147599,33.3080171', name: 'Asker Durağı.'
+		},
+		{cord: '35.2124484,33.3108293', name: 'Büyük Kiler Durağı.'
+		},
+		{cord: '35.2104944,33.3140701', name: 'Deniz Kırtasiye Durağı.'
+		},
+		{cord: '35.2093752,33.3183918', name: 'Küçük Kiler Durağı.'
+		},
+		{cord: '35.2127005,33.3343237', name: 'Fuar Durağı.'
+		},
+		{cord: '35.2263135,33.3261945', name: 'YDÜ Hukuk Fakültesi Durağı.'
+		},
+		{cord: '35.2297766,33.3243684', name: 'YDÜ Kampüs Ana Durak (8.Yurt Önü).'
+		}
+	],
+    
+	'Gönyeli 2': [
+		{cord: '35.2297766,33.3243684', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
+		},
+		{cord: '35.2260295,33.3256762', name: 'Near East Bank Durağı.'
+		},
+		{cord: '35.2100339,33.3149912', name: 'Zorba Petrol Durağı.'
+		},
+		{cord: '35.2124484,33.3108293', name: 'Büyük Kiler Durağı.'
+		},
+		{cord: '35.21818,33.303513', name: 'Yalçın Park Durağı.'
+		},
+		{cord: '35.2218081,33.3008818', name: 'Livera Öğrenci Yurdu Durağı.'
+		},
+		{cord: '35.2256934,33.2983684', name: 'Gönyeli İlkokulu Durağı.'
+		},
+		{cord: '35.2263135,33.3261945', name: 'YDÜ Hukuk Fakültesi Durağı'
+		},
+		{cord: '35.2297766,33.3243684', name: 'YDÜ Kampüs Ana Durak (8.Yurt Önü).'
+		}
+	],
+    
+	'Nicosia 2': [
+		{cord: '35.229767,33.3245232', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
+		},
+		{cord: '35.2260569,33.3256555', name: 'Near East Bank Durağı.'
+		},
+		{cord: '35.1823327,33.3624931', name: 'Girne Kapısı ( Mücahitler Parkı Önü ).'
+		},
+		{cord: '35.1860322,33.3573311', name: 'Kumsal Park Durağı.'
+		},
+		{cord: '35.1908136,33.3520042', name: 'Merit Durağı.'
+		},
+		{cord: '35.1940996,33.3481617', name: 'Sağlık Bakanlığı Durağı.'
+		},
+		{cord: '35.1996941,33.3409093', name: 'Tepe Home Durağı.'
+		},
+		{cord: '35.2050624,33.3302729', name: 'Dr. Burhan Nalbantoğlu Devlet Hastanesi Durağı.'
+		},
+		{cord: '35.226314,33.326203', name: 'YDÜ Hukuk Fakültesi Durağı.'
+		},
+		{cord: '35.2297654,33.3246926', name: 'YDÜ Kampüs Ana Durak (8.Yurt Önü).'
+		}
+	],
+    
+	'Kyrenia': [
+		{cord: '35.2296652,33.323864', name: 'YDÜ.'
+		},
+		{cord: '35.3343279,33.3404879', name: 'Girne Üniversitesi.'
+		}
+	],
+    
+	'Güzelyurt': [
+		{cord: '35.1929487,32.994725', name: 'YDÜ Kampüs.'
+		},
+		{cord: '35.2297531,33.3246818', name: 'Eğitim Sarayı Durağı.'
+		}
+	],
+    
+	'Famagusta': [
+		{ cord: '35.1204568,33.9375611', name:' Mağusa Anıt Çemberi İtimat Yazıhanesi.'
+		},
+		{ cord: '35.2297399,33.3238977', name:'YDÜ Kampüs.'
+		}
+	],
+	'Kızılbaş': [
+		{cord: '35.2297577,33.3245279', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
+		},
+		{cord: '35.2260788,33.3256714', name: 'Near East Bank Durağı.'
+		},
+		{cord: '35.2030437,33.3513253', name: 'Devpa Durağı.'
+		},
+		{cord: '35.1993352,33.3571771', name: 'Domuzcular Burnu Durağı.'
+		},
+		{cord: '35.2023729,33.3635345', name: 'Ebu Bekir Cami Durağı.'
+		},
+		{cord: '35.2035272,33.3689073', name: 'Kampanya Market Durağı.'
+		},
+		{cord: '35.2033482,33.3733046', name: 'Belediye Pazarı Durağı.'
+		},
+		{cord: '35.2029679,33.3767415', name: 'Atatürk Öğretmen Akademisi Durağı.'
+		},
+		{cord: '35.2087216,33.3786829', name: 'Dumlupınar Muhtarlığı Durağı.'
+		},
+		{cord: '35.2112621,33.3756354', name: 'Cadde Mutfak Durağı.'
+		},
+		{cord: '35.2101484,33.3652874', name: 'Kıbrıs Gazetesi Durağı.'
+		},
+		{cord: '35.2096534,33.346799', name: 'Genç Tv Durağı.'
+		},
+		{cord: '35.2262949,33.3262178', name: 'YDÜ Hukuk Fakültesi Durağı.'
+		},
+		{cord: '35.2297615,33.3246945', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
+		}
     ],
     
-    'Gönyeli 1': [
-        {cord: "35.2297781,33.3242986”, name: YDÜ Kampüs Ana Durak (8. Yurt Önü).
-}
-{cord: "35.2260295,33.3256762”, name: Near East Bank Durağı.
-}
-{cord: "35.2095115,33.2983604”, name: İR-MAR Market Karşı Durağı.
-}
-{cord: "35.2087531,33.3074487”, name: Talihsiz Mobilya Durağı.
-}
-{cord: "35.2118788,33.3071764”, name: Ersoy Hoca Apt. Durağı.
-}
-{cord: "35.2147599,33.3080171”, name: Asker Durağı.
-}
-{cord: "35.2124484,33.3108293”, name: Büyük Kiler Durağı.
-}
-{cord: "35.2104944,33.3140701”, name: Deniz Kırtasiye Durağı.
-}
-{cord: "35.2093752,33.3183918”, name: Küçük Kiler Durağı.
-}
-{cord: "35.2127005,33.3343237”, name: Fuar Durağı.
-}
-{cord: "35.2263135,33.3261945”, name: YDÜ Hukuk Fakültesi Durağı.
-}
-{cord: "35.2297766,33.3243684”, name: YDÜ Kampüs Ana Durak (8.Yurt Önü).
-}
+    'Gönyeli / Metehan': [
+    {cord: '35.2297536,33.3245281', name: YDÜ Kampüs Ana Durak (8. Yurt Önü).
+},
+{cord: '35.226102,33.3256871' name: Near East Bank Durağı.
+},
+{cord: '35.2085481,33.3185105', name: Sismar Durağı.
+},
+{cord: '35.2079478,33.3141112', name: Üst Geçit Durağı.
+},
+{cord: '35.2080545,33.3073357', name: Arda Market Durağı.
+},
+{cord: '35.210114,33.2933283”' name: Sarıtaş Evler Durağı.
+},
+{cord: '35.1983067,33.3116735', name: Polis Akademisi Durağı.
+},
+{cord: '35.1937856,33.3211279', name: Zephyr Durağı.
+},
+{cord: '35.1914079,33.3256688', name: Kiler Durağı.
+},
+{cord: '35.2017902,33.3222991', name: Damdelen Durağı.
+},
+{cord: '35.204979,33.3172657' name: Gönyeli Belediyesi Durağı.
+},
+{cord: '35.2088117,33.3181717', name:  Skylap Apt. Durağı.
+},
+{cord: '35.2263203,33.3262207', name:Hukuk Fakültesi Durağı.
+},
+{cord: '35.2297565,33.3246929', name: YDÜ Kampüs Ana Durak (8. Yurt Önü).
+},
 
     ]
 
