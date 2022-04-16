769c848c74630167ee9501fca01afe882aaebe96
kromate <akpananthony33@gmail.com>
2022-04-16 14:17:08 +0100

    --- Auto-Git Commit ---

--- src/helpers/busRoutes.js
+++ src/helpers/busRoutes.js
@@ -18,6 +18,59 @@ export const AllBusRoute = {
 		},
 		{cord: '35.2297654,33.3246926', name: 'YDÜ Kampüs Ana Durak (8.Yurt Önü)'
 		},
-	]
+	],
+    
+	'Ortaköy / Yenikent': [
+		{cord: '35.2297737,33.3246781', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
+		},
+		{cord: '35.2259681,33.325705', name:' Near East Bank Durağı.'
+		},
+		{cord: '35.2077291,33.3399972', name: 'Demir Market Durağı.'
+		},
+		{cord: '35.2024341,33.3420572', name: 'Yeni Yüzyıl Okul Durağı.'
+		},
+		{cord: '35.2005981,33.3401994', name: 'Ortaköy Işıkları Durağı.'
+		},
+		{cord: '35.1986166,33.3338206', name: 'Lemar Durağı.'
+		},
+		{cord: '35.1969042,33.3310818', name: 'Sivil Savunma Durağı.'
+		},
+		{cord: '35.1990717,33.3272494', name: 'Levent Apt. Durağı.'
+		},
+		{cord: '35.2010534,33.3238054', name: 'Arçelik Durağı.'
+		},
+		{cord: '35.2263282,33.3261837', name: 'YDÜ Hukuk Fakültesi Durağı.'
+		},
+		{cord: '35.2297737,33.3246781', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü).'
+		},
+    ],
+    
+    'Gönyeli 1': [
+        {cord: "35.2297781,33.3242986”, name: YDÜ Kampüs Ana Durak (8. Yurt Önü).
+}
+{cord: "35.2260295,33.3256762”, name: Near East Bank Durağı.
+}
+{cord: "35.2095115,33.2983604”, name: İR-MAR Market Karşı Durağı.
+}
+{cord: "35.2087531,33.3074487”, name: Talihsiz Mobilya Durağı.
+}
+{cord: "35.2118788,33.3071764”, name: Ersoy Hoca Apt. Durağı.
+}
+{cord: "35.2147599,33.3080171”, name: Asker Durağı.
+}
+{cord: "35.2124484,33.3108293”, name: Büyük Kiler Durağı.
+}
+{cord: "35.2104944,33.3140701”, name: Deniz Kırtasiye Durağı.
+}
+{cord: "35.2093752,33.3183918”, name: Küçük Kiler Durağı.
+}
+{cord: "35.2127005,33.3343237”, name: Fuar Durağı.
+}
+{cord: "35.2263135,33.3261945”, name: YDÜ Hukuk Fakültesi Durağı.
+}
+{cord: "35.2297766,33.3243684”, name: YDÜ Kampüs Ana Durak (8.Yurt Önü).
+}
+
+    ]
 
 }
