92f6bd77f37ea1b6c6d8c921158fd663734a4e39
kromate <akpananthony33@gmail.com>
2022-04-16 14:02:02 +0100

    --- Auto-Git Commit ---

---
+++ src/helpers/busRoutes.js
@@ -0,0 +1,23 @@
+export const AllBusRoute = {
+	'Nicosia 1': [
+		{ cord: '35.2297654,33.3246926', name: 'YDÜ Kampüs Ana Durak (8. Yurt Önü)'
+		},
+		{cord: '35.2260569,33.3256555', name: 'Near East Bank Durağı'
+		},
+		{cord: '35.2095435,33.3667254', name: 'Honda Durağı'
+		},
+		{cord: '35.2021241,33.367465', name: 'Çangar Durağı'
+		},
+		{cord: '35.1990897,33.3678455', name: 'Reis Supermarket Karşısı'
+		},
+		{cord: '35.1948409,33.3678284', name: 'Kaymaklı Eski Mezarlık Durağı'
+		},
+		{cord: '35.1880314,33.3660735', name: 'City Royal Hotel Terminal Durağı'
+		},
+		{cord: '35.226314,33.326203', name: 'YDÜ Hukuk Fakültesi Durağı'
+		},
+		{cord: '35.2297654,33.3246926', name: 'YDÜ Kampüs Ana Durak (8.Yurt Önü)'
+		},
+	]
+
+}
