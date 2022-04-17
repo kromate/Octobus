15deead827de117024e1d3ef3a65a47d1568926f
kromate <akpananthony33@gmail.com>
2022-04-17 15:46:02 +0100

    feat: change font style

--- src/components/BottomModal.vue
+++ src/components/BottomModal.vue
@@ -1,11 +1,16 @@
 <template>
 	<transition name="slideDown">
-		<div class="w-screen h-8" v-if="open">
-
+		<div class="w-screen h-96 shadow-modal bg-white  z-40 fixed top-0 p-4" v-if="open">
+			<div class="flex items-center">
+				<i class="fas fa-times text-primary text-left text-2xl"></i>
+				<p class="font-bold ml-8 text-xl">
+					Select destination
+				</p>
+			</div>
 		</div>
 
 	</transition>
-	<div :class="[ open ?'h-screen rounded-none top-0':'rounded-t-[4px]', 'px-3 z-50 fixed  py-4 bottom-12  inset-x-0 bg-white justify-between w-full animated ' ]" >
+	<div :class="[ open ?'h-screen rounded-none top-0':'rounded-t-[4px]', 'px-3 z-30 fixed  py-4 bottom-12  inset-x-0 bg-white justify-between w-full animated ' ]" >
 		<div :class="[ open?'hidden':'',  'w-14 h-1.5 rounded bg-gray mx-auto']"/>
 	
 		<div class="relative" @click="toggleModal">
@@ -26,7 +31,7 @@ export default {
 
 	setup(){
 		const open = ref(false)
-		const toggleModal = (el)=>{
+		const toggleModal = ()=>{
 			console.log(open.value)
 			// if(el.target.className.includes('open')){
 			open.value = !open.value
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  mode:"jit",
-  theme: {
-  extend: {
-     spacing: {
-      128: '32rem',
-     },
-    colors:{
-     primary:'#000000',
-     secondary:'#8F2A46',
-     lightGray:'#F6F6F6',
-     gray:'#A4A8BA',
-     blue:'#2c58de0d',
-     }
-   },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	mode:'jit',
+	theme: {
+		extend: {
+			spacing: {
+				128: '32rem',
+			},
+			colors:{
+				primary:'#000000',
+				secondary:'#8F2A46',
+				lightGray:'#F6F6F6',
+				gray:'#A4A8BA',
+				blue:'#2c58de0d',
+			},
+			boxShadow: {
+				modal: '0 -9px 3px 11px rgba(2, 2, 2, 0.25)'
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
