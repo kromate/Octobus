eba54de14b8ddc58b49c01557c1b65f35bf33492
kromate <akpananthony33@gmail.com>
2022-04-17 16:01:49 +0100

    feat: update

--- src/components/BottomModal.vue
+++ src/components/BottomModal.vue
@@ -2,10 +2,21 @@
 	<transition name="slideDown">
 		<div class="w-screen h-96 shadow-modal bg-white  z-40 fixed top-0 p-4" v-if="open">
 			<div class="flex items-center">
-				<i class="fas fa-times text-primary text-left text-2xl"></i>
+				<i class="fas fa-times text-primary text-left text-2xl cursor-pointer" @click="toggleModal"></i>
 				<p class="font-bold ml-8 text-xl">
 					Select destination
 				</p>
+			</div>
+			<div class="flex items-center mt-3 w-full">
+				<div class="flex flex-col h-full items-center mr-3">
+					<div class="border-[7px] border-green-800 rounded-full w-6 h-6"/>
+					<div class="h-full min-h-[20px] border border-[#c6c2c2] w-0 my-0.5"/>
+					<div class="border-[7px] border-red-800 rounded-full w-6 h-6"/>
+				</div>
+				<div class="flex flex-col w-full">
+					<input type="text" placeholder="Your Location" class="input">
+					<input type="text" placeholder="Search Location" class="mt-2 input">
+				</div>
 			</div>
 		</div>
 
