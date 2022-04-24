9dee957b927b8d47476cb7ad2c77d451d4c574ab
kromate <akpananthony33@gmail.com>
2022-04-24 21:46:32 +0100

    --- Auto-Git Commit ---

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -7,11 +7,25 @@
 			</button>
 		</div>
 	
-		<select-menu placeholder='route'  :options="routeNamesOnly">
+		<select-menu placeholder='route'  :options="routeNamesOnly" class="mb-10">
 			<template #icon>
 				<i class="fas fa-directions text-primary text-xl"></i>
 			</template>
 		</select-menu>
+
+		<div class="p-2 bg-gray mt-2 rounded">
+			<span v-if="!sender">{{ name }}</span>
+			<div class="flex" :class="sender ? 'flex-row-reverse' : ''">
+				<img src="../assets/people/1.png" alt="person" class="rounded-full mr-4">
+				<div>
+					<div class="text w-3/4" :class="sender ? 'bg-green-800' : 'bg-gray-700'">
+						there is going to be delay at the Nicosia 1 route, I have been here for hours no bus
+					</div>
+					<span class="text-xs italic">Date: 3 mins ago</span>
+				</div>
+			
+			</div>
+		</div>
 	</div>
 
 </template>
