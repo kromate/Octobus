5ad4c8841e7f383dcd768b8bf6bca3e55f9e1d5d
kromate <akpananthony33@gmail.com>
2022-04-26 17:13:20 +0100

    --- Auto-Git Commit ---

--- src/composables/useDistance.js
+++ src/composables/useDistance.js
@@ -46,7 +46,17 @@ function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
 	const d = R * c // Distance in km
 	return d
 }
+function haversine_distance(mk1, mk2) {
+	const R = 3958.8 // Radius of the Earth in miles
+	const rlat1 = mk1.position.lat() * (Math.PI/180) // Convert degrees to radians
+	const rlat2 = mk2.position.lat() * (Math.PI/180) // Convert degrees to radians
+	const difflat = rlat2-rlat1 // Radian difference (latitudes)
+	const difflon = (mk2.position.lng()-mk1.position.lng()) * (Math.PI/180) // Radian difference (longitudes)
 
+	const d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)))
+	return d
+}
+	
 function deg2rad(deg) {
 	return deg * (Math.PI/180)
 }
