b81f9c4944e2aa9c6f69490898f4e323ecd3cafb
kromate <akpananthony33@gmail.com>
2022-05-12 17:29:33 +0100

    --- Auto-Git Commit ---

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {registerComponents} from './plugin/registerGlobally'
+// import { registerComponents } from './plugin/registerGlobally'
 import { router as setupRouter } from '@/router'
 import gAuthPlugin from 'vue3-google-oauth2'
 
 import './assets/style/main.css'
 
-const init = async ()=>{
+const init = async () => {
 	const router = await setupRouter
-	console.log(router.isReady())
-	await router.isReady()
+
 	const app = createApp(App)
 	app.use(gAuthPlugin, { clientId: '394838939483-rq7d2rfj39gkdfjd9jenu670ounoi01.apps.googleusercontent.com', scope: 'email', prompt: 'consent', fetch_basic_profile: false })
-	await registerComponents(app)
+
 	app.use(router).mount('#app')
 }
 
