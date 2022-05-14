e76729add45c015ed70792af2b290df59a863930
kromate <akpananthony33@gmail.com>
2022-05-14 18:24:55 +0100

    --- Auto-Git Commit ---

---
+++ src/views/routes/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<topbar name="Profile"/>
+	<home-page>
+		<div class="bg-lightPurplebg min-h-screen  py-8 flex flex-col pb-24">
+		
+
+			<div class="flex items-center py-4 border-b border-lightGray px-4 gap-4">
+				<i class="fas fa-edit  text-2xl"></i>
+				<span class="text-base font-normal">Edit profile</span>
+			</div>
+
+	
+	
+		</div>
+
+
+
+	
+	</home-page>
+	
+</template>
+
+<script setup>
+
+import homePage from '@/layouts/homePage.vue'
+import Topbar from '@/components/Topbar.vue'
+import { useRouter } from 'vue-router'
+
+
+
+
+
+
+
+
+</script>
+
+
