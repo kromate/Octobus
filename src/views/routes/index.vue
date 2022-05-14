604519a315b98cce03ee9cda71f7e08edcf8b1d1
kromate <akpananthony33@gmail.com>
2022-05-14 18:39:17 +0100

    --- Auto-Git Commit ---

--- src/views/routes/index.vue
+++ src/views/routes/index.vue
@@ -1,12 +1,13 @@
 <template>
-	<topbar name="Profile"/>
+
 	<home-page>
-		<div class="bg-lightPurplebg min-h-screen  py-8 flex flex-col pb-24">
+		<topbar name="Profile"/>
+		<div class="bg-lightPurplebg min-h-screen  py-8 flex flex-col pb-24 mt-10">
 		
 
-			<div class="flex items-center py-4 border-b border-lightGray px-4 gap-4">
-				<i class="fas fa-edit  text-2xl"></i>
+			<div class="flex items-center justify-between py-3.5 border-b border-lightGray px-4 gap-4">
 				<span class="text-base font-normal">Edit profile</span>
+				<i class="fas fa-arrow-right  text-2xl"></i>
 			</div>
 
 	
