679498a561f252af64aaf952078a08c204f9d8fd
kromate <akpananthony33@gmail.com>
2022-05-15 22:17:43 +0100

    --- Auto-Git Commit ---

--- src/views/profile/contact.vue
+++ src/views/profile/contact.vue
@@ -9,17 +9,17 @@
 		<div class="bg-lightPurplebg min-h-screen  py-8 flex flex-col pb-24" >
 			<div class="flex flex-col w-full h-full  items-center px-4">
 
-				<div class="flex md:flex-row  flex-col gap-20 mt-12 text-xl">
+				<div class="flex md:flex-row  flex-col gap-4 mt-12 text-xl w-full">
 					<div class="flex flex-col gap-5">
-						<span> Jeremiah Godwin </span>
-						<span> 	+14044035611  </span>
-						<span> 	20204960@std.neu.tr</span>
+						<span><i class="fas fa-user  text-2xl mx-auto "></i> Jeremiah Godwin </span>
+						<span><i class="fas fa-phone  text-2xl mx-auto "></i> 	+14044035611  </span>
+						<span> <i class="fas fa-envelope  text-2xl mx-auto "></i>	20204960@std.neu.tr</span>
 					</div>
-
+					<div class="bg-secondary h-1 w-full"/>
 					<div class="flex flex-col gap-5">
-						<span>	Julia Mayoko</span>
-						<span>	+90 533 835 09 86</span>
-						<span>	20166299@std.neu.edu.tr</span>	
+						<span><i class="fas fa-user  text-2xl mx-auto "></i>	Julia Mayoko</span>
+						<span><i class="fas fa-phone  text-2xl mx-auto "></i>	+90 533 835 09 86</span>
+						<span><i class="fas fa-envelope  text-2xl mx-auto "></i>	20166299@std.neu.edu.tr</span>	
 					</div>
 				</div>
 			</div>
