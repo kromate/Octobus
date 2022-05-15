29c0ba8343e1915e523483183e0f6dc9d8cbd51e
kromate <akpananthony33@gmail.com>
2022-05-15 13:32:56 +0100

    --- Auto-Git Commit ---

---
+++ src/views/routes/_id/bus-stops.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="bg-secondary text-white fixed z-10 top-0 inset-x-0 w-full h-12 min-h-[7vh] items-center justify-center flex shadow px-4 py-2 md:px-8">
+		<i class="fas fa-arrow-left text-xl absolute left-4" @click="$router.go(-1)"></i>
+		<h3 class="text-base font-medium">
+			{{selectedService}}
+		</h3>
+	
+	</div>
+	<home-page>
+
+		<RotueModal :route="selectedService"/>
+	</home-page>
+
+</template>
+
+
+
+<script setup>
+import {ref} from 'vue'
+import homePage from '@/layouts/homePage.vue'
+import { useRoute } from 'vue-router'
+import RotueModal from '@/components/RouteModal.vue'
+
+const selectedService = ref('Nicosia 1')
+
+selectedService.value = useRoute().params.id
+
+
+
+</script>
+
+
+<style>
+/* .map{
+  height: calc(100vh - 96px);
+}
+
+.inputContainer i {
+   position: absolute;
+} */
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	content: [
-		'./index.html',
-		'./src/**/*.{vue,js,ts,jsx,tsx}',
+		'./src/components/**/*.{vue,js,ts}',
+		'./src/views/**/*.{vue,js,ts}',
 	],
 	mode: 'jit',
 	theme: {
